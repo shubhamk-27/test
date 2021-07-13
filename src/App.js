354bd9574932d9fb47a6f1139ec596cd824cd52b
Shubham Koli <shubhamkoli1927@gmail.com>
2021-07-13 15:26:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [formName, setFormName] = useState("");
+  const [formEmail, setFormEmail] = useState("");
+
+  const displayInformation = (e) => {
+    e.preventDefault();
+    setFormName(name);
+    setFormEmail(email);
+  };
+
+  const deleteInformation = () => {
+    setFormName("");
+    setFormEmail("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div className="title">
+          <h1>Username</h1>
+          <input
+            className="input"
+            type="text"
+            name="username"
+            placeholder="Enter Username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="title">
+          <h1>Email</h1>
+          <input
+            className="input"
+            type="text"
+            name="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <button className="button" onClick={displayInformation}>
+          Submit Info
+        </button>
+        <button className="button" onClick={deleteInformation}>
+          Delete Info
+        </button>
+      </form>
+      <div>
+        <h1>Name : {formName}</h1>
+        <h1>Email : {formEmail}</h1>
+      </div>
     </div>
   );
 }
